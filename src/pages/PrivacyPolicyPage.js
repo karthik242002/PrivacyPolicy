1306c5b857f90fbf9b73fc4202c6cbbec9074db4
karthik242002 <karthik242002>
2023-09-28 10:41:18 +0000

    First Commit from Locofy Builder

---
+++ src/pages/PrivacyPolicyPage.js
@@ -0,0 +1,410 @@
+import { useCallback } from "react";
+import "./PrivacyPolicyPage.css";
+
+const PrivacyPolicyPage = () => {
+  const onLinkText1Click = useCallback(() => {
+    // Please sync "Home Page" to the project
+  }, []);
+
+  const onLinkText2Click = useCallback(() => {
+    // Please sync "About Us Page" to the project
+  }, []);
+
+  const onLinkText3Click = useCallback(() => {
+    // Please sync "Contact page" to the project
+  }, []);
+
+  const onMenuItemContainer4Click = useCallback(() => {
+    // Please sync "Food Products Page" to the project
+  }, []);
+
+  const onMenuItemContainer5Click = useCallback(() => {
+    // Please sync "Religious SrorePage" to the project
+  }, []);
+
+  const onMenuItemContainer6Click = useCallback(() => {
+    // Please sync "Books Page" to the project
+  }, []);
+
+  const onMenuItemContainer7Click = useCallback(() => {
+    // Please sync "Subsidized Food page" to the project
+  }, []);
+
+  return (
+    <div className="privacy-policy-page">
+      <div className="header-46">
+        <div className="content39">
+          <b className="heading5">Privacy Policy</b>
+          <div className="text2">Effective date: April 3, 2023</div>
+        </div>
+        <div className="component-3">
+          <div className="navbar-22">
+            <div className="container4">
+              <div className="logo-holder4">
+                <div className="logo4">
+                  <img
+                    className="image-4-icon4"
+                    alt=""
+                    src="/image-42@2x.png"
+                  />
+                </div>
+                <div className="link52">Nourishing Lives, Enriching Souls</div>
+              </div>
+              <div className="column8">
+                <div className="button5">
+                  <div className="link52">Login</div>
+                </div>
+              </div>
+              <div className="column9">
+                <div className="link53" onClick={onLinkText1Click}>
+                  Home
+                </div>
+                <div className="link53" onClick={onLinkText2Click}>
+                  About Us
+                </div>
+                <div className="link53" onClick={onLinkText3Click}>
+                  Contact Us
+                </div>
+                <div className="nav-link-dropdown2">
+                  <div className="link52">Products</div>
+                  <img
+                    className="chevron-down-icon2"
+                    alt=""
+                    src="/chevron-down.svg"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="menu3">
+            <div className="menu-list6">
+              <div className="page-group-one3">Page group one</div>
+              <div className="menu-item24">
+                <img
+                  className="chevron-down-icon2"
+                  alt=""
+                  src="/icon--relume.svg"
+                />
+                <div className="content40">
+                  <div className="page-one3">Page One</div>
+                  <div className="lorem-ipsum-dolor24">
+                    Lorem ipsum dolor sit amet consectetur elit
+                  </div>
+                </div>
+              </div>
+              <div className="menu-item24">
+                <img
+                  className="chevron-down-icon2"
+                  alt=""
+                  src="/icon--relume.svg"
+                />
+                <div className="content40">
+                  <div className="page-one3">Page Two</div>
+                  <div className="lorem-ipsum-dolor24">
+                    Lorem ipsum dolor sit amet consectetur elit
+                  </div>
+                </div>
+              </div>
+              <div className="menu-item24">
+                <img
+                  className="chevron-down-icon2"
+                  alt=""
+                  src="/icon--relume.svg"
+                />
+                <div className="content40">
+                  <div className="page-one3">Page Three</div>
+                  <div className="lorem-ipsum-dolor24">
+                    Lorem ipsum dolor sit amet consectetur elit
+                  </div>
+                </div>
+              </div>
+              <div className="menu-item24">
+                <img
+                  className="chevron-down-icon2"
+                  alt=""
+                  src="/icon--relume.svg"
+                />
+                <div className="content40">
+                  <div className="page-one3">Page Four</div>
+                  <div className="lorem-ipsum-dolor24">
+                    Lorem ipsum dolor sit amet consectetur elit
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="menu-list6">
+              <div className="page-group-one3">Page group two</div>
+              <div className="menu-item28" onClick={onMenuItemContainer4Click}>
+                <img
+                  className="chevron-down-icon2"
+                  alt=""
+                  src="/icon--relume.svg"
+                />
+                <div className="content40">
+                  <div className="page-one3">Page Five</div>
+                  <div className="lorem-ipsum-dolor24">
+                    Lorem ipsum dolor sit amet consectetur elit
+                  </div>
+                </div>
+              </div>
+              <div className="menu-item28" onClick={onMenuItemContainer5Click}>
+                <img
+                  className="chevron-down-icon2"
+                  alt=""
+                  src="/icon--relume.svg"
+                />
+                <div className="content40">
+                  <div className="page-one3">Page Six</div>
+                  <div className="lorem-ipsum-dolor24">
+                    Lorem ipsum dolor sit amet consectetur elit
+                  </div>
+                </div>
+              </div>
+              <div className="menu-item28" onClick={onMenuItemContainer6Click}>
+                <img
+                  className="chevron-down-icon2"
+                  alt=""
+                  src="/icon--relume.svg"
+                />
+                <div className="content40">
+                  <div className="page-one3">Page Seven</div>
+                  <div className="lorem-ipsum-dolor24">
+                    Lorem ipsum dolor sit amet consectetur elit
+                  </div>
+                </div>
+              </div>
+              <div className="menu-item28" onClick={onMenuItemContainer7Click}>
+                <img
+                  className="chevron-down-icon2"
+                  alt=""
+                  src="/icon--relume.svg"
+                />
+                <div className="content40">
+                  <div className="page-one3">Page Eight</div>
+                  <div className="lorem-ipsum-dolor24">
+                    Lorem ipsum dolor sit amet consectetur elit
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="content-27">
+        <div className="content48">
+          <div className="column10">
+            <b className="table-of-contents">Table of contents</b>
+            <div className="content49">
+              <div className="selected">
+                <b className="link52">Heading 2</b>
+              </div>
+              <div className="button7">
+                <div className="link52">Heading 3</div>
+              </div>
+              <div className="button8">
+                <div className="link52">Heading 4</div>
+              </div>
+              <div className="button9">
+                <div className="link52">Heading 5</div>
+              </div>
+              <div className="button10">
+                <div className="link52">Heading 6</div>
+              </div>
+            </div>
+          </div>
+          <div className="rich-text1">
+            <div className="content50">
+              <b className="heading5">Heading 2</b>
+            </div>
+            <div className="content51">
+              <b className="dolor-enim-eu1">
+                Dolor enim eu tortor urna sed duis nulla. Aliquam vestibulum,
+                nulla odio nisl vitae. In aliquet pellentesque aenean hac
+                vestibulum turpis mi bibendum diam. Tempor integer aliquam in
+                vitae malesuada fringilla.
+              </b>
+            </div>
+            <div className="content52">
+              <div className="mi-tincidunt-elit1">
+                Mi tincidunt elit, id quisque ligula ac diam, amet. Vel etiam
+                suspendisse morbi eleifend faucibus eget vestibulum felis.
+                Dictum quis montes, sit sit. Tellus aliquam enim urna, etiam.
+                Mauris posuere vulputate arcu amet, vitae nisi, tellus
+                tincidunt. At feugiat sapien varius id.
+              </div>
+            </div>
+            <div className="content53">
+              <b className="heading5">Heading 3</b>
+            </div>
+            <div className="content52">
+              <div className="mi-tincidunt-elit1">
+                Eget quis mi enim, leo lacinia pharetra, semper. Eget in
+                volutpat mollis at volutpat lectus velit, sed auctor. Porttitor
+                fames arcu quis fusce augue enim. Quis at habitant diam at.
+                Suscipit tristique risus, at donec. In turpis vel et quam
+                imperdiet. Ipsum molestie aliquet sodales id est ac volutpat.
+              </div>
+            </div>
+            <div className="content52">
+              <div className="mi-tincidunt-elit1">
+                Tristique odio senectus nam posuere ornare leo metus, ultricies.
+                Blandit duis ultricies vulputate morbi feugiat cras placerat
+                elit. Aliquam tellus lorem sed ac. Montes, sed mattis
+                pellentesque suscipit accumsan. Cursus viverra aenean magna
+                risus elementum faucibus molestie pellentesque. Arcu ultricies
+                sed mauris vestibulum.
+              </div>
+            </div>
+            <div className="content56">
+              <b className="heading-42">Heading 4</b>
+            </div>
+            <div className="content52">
+              <div className="mi-tincidunt-elit1">
+                Morbi sed imperdiet in ipsum, adipiscing elit dui lectus. Tellus
+                id scelerisque est ultricies ultricies. Duis est sit sed leo
+                nisl, blandit elit sagittis. Quisque tristique consequat quam
+                sed. Nisl at scelerisque amet nulla purus habitasse.
+              </div>
+            </div>
+            <div className="content58">
+              <b className="dolor-enim-eu1">Heading 5</b>
+            </div>
+            <div className="content52">
+              <div className="mi-tincidunt-elit1">
+                Morbi sed imperdiet in ipsum, adipiscing elit dui lectus. Tellus
+                id scelerisque est ultricies ultricies. Duis est sit sed leo
+                nisl, blandit elit sagittis. Quisque tristique consequat quam
+                sed. Nisl at scelerisque amet nulla purus habitasse.
+              </div>
+            </div>
+            <div className="content60">
+              <div className="figure">
+                <div className="divider2" />
+                <i className="ipsum-sit-mattis">
+                  "Ipsum sit mattis nulla quam nulla. Gravida id gravida ac enim
+                  mauris id. Non pellentesque congue eget consectetur turpis.
+                  Sapien, dictum molestie sem tempor. Diam elit, orci, tincidunt
+                  aenean tempus."
+                </i>
+              </div>
+            </div>
+            <div className="content61">
+              <b className="dolor-enim-eu1">Heading 6</b>
+            </div>
+            <div className="content52">
+              <div className="mi-tincidunt-elit1">
+                Nunc sed faucibus bibendum feugiat sed interdum. Ipsum egestas
+                condimentum mi massa. In tincidunt pharetra consectetur sed duis
+                facilisis metus. Etiam egestas in nec sed et. Quis lobortis at
+                sit dictum eget nibh tortor commodo cursus.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer-61">
+        <div className="container5">
+          <div className="divider3" />
+          <div className="links1">
+            <div className="column11">
+              <div className="page-one3">Column One</div>
+              <div className="footer-links4">
+                <div className="link57">
+                  <div className="link58">Link One</div>
+                </div>
+                <div className="link57">
+                  <div className="link58">Link Two</div>
+                </div>
+                <div className="link57">
+                  <div className="link58">Link Three</div>
+                </div>
+                <div className="link57">
+                  <div className="link58">Link Four</div>
+                </div>
+                <div className="link57">
+                  <div className="link58">Link Five</div>
+                </div>
+              </div>
+            </div>
+            <div className="column11">
+              <div className="page-one3">Column Two</div>
+              <div className="footer-links4">
+                <div className="link57">
+                  <div className="link58">Link Six</div>
+                </div>
+                <div className="link57">
+                  <div className="link58">Link Seven</div>
+                </div>
+                <div className="link57">
+                  <div className="link58">Link Eight</div>
+                </div>
+                <div className="link57">
+                  <div className="link58">Link Nine</div>
+                </div>
+                <div className="link57">
+                  <div className="link58">Link Ten</div>
+                </div>
+              </div>
+            </div>
+            <div className="column11">
+              <div className="page-one3">Column Three</div>
+              <div className="footer-links4">
+                <div className="link57">
+                  <div className="link58">Link Eleven</div>
+                </div>
+                <div className="link57">
+                  <div className="link58">Link Twelve</div>
+                </div>
+                <div className="link57">
+                  <div className="link58">Link Thirteen</div>
+                </div>
+                <div className="link57">
+                  <div className="link58">Link Fourteen</div>
+                </div>
+                <div className="link57">
+                  <div className="link58">Link Fifteen</div>
+                </div>
+              </div>
+            </div>
+            <div className="column11">
+              <div className="page-one3">Column Four</div>
+              <div className="footer-links4">
+                <div className="link57">
+                  <div className="link58">Link Sixteen</div>
+                </div>
+                <div className="link57">
+                  <div className="link58">Link Seventeen</div>
+                </div>
+                <div className="link57">
+                  <div className="link58">Link Eightteen</div>
+                </div>
+                <div className="link57">
+                  <div className="link58">Link Nineteen</div>
+                </div>
+                <div className="link57">
+                  <div className="link58">Link Twenty</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="credits1">
+            <div className="divider3" />
+            <div className="logo-holder-group">
+              <div className="logo-holder5">
+                <div className="logo4">
+                  <img className="image-4-icon4" alt="" src="/image-4@2x.png" />
+                </div>
+                <div className="link52">Nourishing Lives, Enriching Souls</div>
+              </div>
+              <div className="text3">
+                © 2023 Hari Arunachala. All rights reserved.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPolicyPage;
